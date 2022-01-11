8b4af660840ba54388e53e16600da3e4824198a2
Bruno-af <bruno.fernandes@ccc.ufcg.edu.br>
2022-01-11 17:59:16 -0300

    Project created

---
+++ backend/src/main/java/com/dsmovie/avalio/AvalioApplication.java
@@ -0,0 +1,13 @@
+package com.dsmovie.avalio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvalioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvalioApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dsmovie/avalio/AvalioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsmovie.avalio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvalioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
